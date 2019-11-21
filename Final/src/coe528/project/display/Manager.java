88a511bdb5aa6876f987762d893fd537428cf35b
GabeCasciano <gabecasciano@gmail.com>
2019-11-21 14:00:29 -0500

    Moving over to netbeans, fuck

---
+++ Final/src/coe528/project/display/Manager.java
@@ -0,0 +1,22 @@
+package coe528.project.display;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Manager implements Initializable {
+
+    @FXML Label accnt_id_number;
+    @FXML Label accnt_owner;
+    @FXML Label accnt_balance;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+
+}
